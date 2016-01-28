7d755b35da9ef957734412ec39631a0f0be6c5b7
MarineLasbleis <marine.lasbleis@gmail.com>
2016-01-28 17:20:41 +0900

    add of file data.py, to modify the data files name.dat that have been provide by G.H. simulations

---
+++ data.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+# Time-stamp: <2016-01-28 17:16:20 marine>
+# Project : Thermal evolution of stratified core
+# Subproject : Read (and correct/interpolate/etc.) data sets given by G.H. 
+# Author : Marine Lasbleis
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
