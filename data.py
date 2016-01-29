c41050db1dec6f3daf769c0d06ab5b9b05afed73
MarineLasbleis <marine.lasbleis@gmail.com>
2016-01-29 22:28:58 +0900

    Add data file with only 2 coeffs (and not 4). Fit is npt as good, but maybe easier to use

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
-# Time-stamp: <2016-01-29 14:47:50 marine>
+# Time-stamp: <2016-01-29 22:22:53 marine>
 # Project : Thermal evolution of stratified core
 # Subproject : Read (and correct/interpolate/etc.) data sets given by G.H. 
 # Author : Marine Lasbleis
@@ -26,6 +26,7 @@ def homogeneous_sample_data(basename="earth", folder="./dat/", Npoints=20):
 
     filename = folder+basename+".dat"
     output_file1 = folder+"polynomes_"+basename+".dat"
+    output_file3 = folder+"polynomes_2coeff_"+basename+".dat"
     output_file2 = folder+"resampled_"+basename+".dat"
     
     fig, ax = plt.subplots(3, 2, sharex=True)
@@ -90,7 +91,33 @@ def homogeneous_sample_data(basename="earth", folder="./dat/", Npoints=20):
     polynomes_all[0:len(polynomes_gravity),2] = polynomes_gravity
     text = "Polynomial coefficients pour Temperature (K), Pressure (Pa), Gravity (m/s**2) as function of radius (m). Highest power first."
     np.savetxt(output_file1, polynomes_all, header=text)
+
+    r = np.linspace(Radius[0], Radius[-1], Npoints)
+    temp = np.polyval(polyn_temp, r)*r**2+T0
+    press = np.polyval(polyn_press, r)*r**2+P0
+    
+    ax[0,0].plot(r/1e3, temp, 'g')
+    ax[1,0].plot(r/1e3, press, 'g')
+    ax[0,1].plot(r/1e3, np.polyval(polyn_temp, r), 'g')
+    ax[1,1].plot(r/1e3, np.polyval(polyn_press, r), 'g')
+
     print "Output file with polynomial coefficients: ", output_file1
+    print "Number of coefficients: ", maxlength
+    print "==="
+
+    polyn_temp = np.polyfit(Radius[1:], (Temperature[1:] -(T0))/Radius[1:]**2., 1)
+    polyn_press = np.polyfit(Radius[1:], (Pressure[1:] -(P0))/Radius[1:]**2., 1)
+    polynomes_temp = np.append(np.array([T0, 0]), polyn_temp)
+    polynomes_press = np.append(np.array([P0, 0]), polyn_press)
+    maxlength = len(polynomes_temp)
+    polynomes_all = np.zeros((maxlength, 3))
+    polynomes_all[0:len(polynomes_temp),0] = polynomes_temp
+    polynomes_all[0:len(polynomes_press),1] = polynomes_press
+    polynomes_all[0:len(polynomes_gravity),2] = polynomes_gravity
+
+    text = "Polynomial coefficients pour Temperature (K), Pressure (Pa), Gravity (m/s**2) as function of radius (m). Highest power first."
+    np.savetxt(output_file3, polynomes_all, header=text)
+    print "Output file with polynomial coefficients - 2coeffs only : ", output_file1
     print "Number of coefficients: ", maxlength
     print "==="
 
@@ -100,19 +127,19 @@ def homogeneous_sample_data(basename="earth", folder="./dat/", Npoints=20):
     press = np.polyval(polyn_press, r)*r**2+P0
     grav = np.polyval(polyn_grav, r)*r
     
-    ax[0,0].plot(r/1e3, temp)
-    ax[1,0].plot(r/1e3, press)
-    ax[2,0].plot(r/1e3, grav)
-    ax[0,1].plot(r/1e3, np.polyval(polyn_temp, r))
-    ax[1,1].plot(r/1e3, np.polyval(polyn_press, r))
-    ax[2,1].plot(r/1e3, np.polyval(polyn_grav, r))
+    ax[0,0].plot(r/1e3, temp, 'b')
+    ax[1,0].plot(r/1e3, press, 'b')
+    ax[2,0].plot(r/1e3, grav, 'b')
+    ax[0,1].plot(r/1e3, np.polyval(polyn_temp, r), 'k')
+    ax[1,1].plot(r/1e3, np.polyval(polyn_press, r), 'k')
+    ax[2,1].plot(r/1e3, np.polyval(polyn_grav, r), 'k')
     ax[2,1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
     
 
     data_resample = np.concatenate((np.array([r]).T, np.array([temp]).T, np.array([press]).T, np.array([grav]).T), axis=1)
     np.savetxt(output_file2, data_resample, header="radius (m), temperature (K)")
-    print "Output file with resample data: ", output_file2
+    print "Output file with resample data ", output_file2
     print "Number of points in radius: ", Npoints
     print "==="
 
