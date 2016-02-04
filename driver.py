22ebeaeb6085eedf603b1470a4f9fb78d058270f
Matthieu Laneuville <mlaneuville@gmail.com>
2016-02-04 16:31:09 +0900

    avoid parallel threads overwriting each others config file

--- driver.py
+++ driver.py
@@ -1,19 +1,20 @@
 import subprocess
+import time
 import logging
 import threading
 import yaml
 import os
 
 # batch run information
-PARALLEL_JOBS = 2   # max number of parallel jobs
-TOTAL_RUNS = 20     # total number of runs in the series
+PARALLEL_JOBS = 3   # max number of parallel jobs
+TOTAL_RUNS = 11     # total number of runs in the series
 
 # null pointer to discard direct code output
 FNULL = open(os.devnull, "w")
 
 # loads default config file structure
 # TODO: maybe we should actually call that one default.yaml
-stream = file("config.yaml", "r")
+stream = file("default.yaml", "r")
 config = yaml.load(stream)
 stream.close()
 
@@ -37,17 +38,16 @@ def generate_config(i):
     '''Example of config generation. Anything can be done here, the point is just
     to generate a new config.yaml file for each run.'''
 
-    config['run_name'] = 'earth_%s' % i
-    config['body'] = 'earth'
-    config['constant_diff'] = 'true'
-    config['constant_diff_value'] = 1e-5*(1+0.5*int(i))
+    TM = 4000+100*int(i)
+    config['run_name'] = 'earth-Tc%d' % TM
+    config['mantle_temperature'] = TM
 
     stream = file('config.yaml', 'w')
     yaml.dump(config, stream)
 
 def worker(s, pool):
     name = threading.currentThread().getName()
-    logging.debug('Worker %s waiting to join the pool' % name)
+    logging.debug('Worker %02d waiting to join the pool' % int(name))
     with s:
         generate_config(name)
         pool.makeActive(name)
@@ -58,4 +58,5 @@ pool = ActivePool()
 s = threading.Semaphore(PARALLEL_JOBS)
 for i in range(TOTAL_RUNS):
     t = threading.Thread(target=worker, name=str(i), args=(s, pool))
+    time.sleep(0.2) # to avoid workers overwriting each other's config file
     t.start()
