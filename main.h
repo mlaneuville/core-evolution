430173464183ad8b54cc0164336f94983f2de1e2
Matthieu Laneuville <mlaneuville@gmail.com>
2016-02-08 16:08:05 +0900

    new dt criteria to consider large CMB heat flow

--- main.h
+++ main.h
@@ -34,4 +34,4 @@ double dt = 0.01*pow(dx,2)/1e-5; // will be updated as soon as actual diffusivit
 
 double R = 3480e3; // core size [m]
 double TBL = 100e3; // thermal boundary layer thickness
-
+double shell_volume = 4*PI*(pow(R,3)-pow(R-R*dx,3))/3; //  volume of the top shell, for dt calculation
