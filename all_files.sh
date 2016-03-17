f88857a3cda0dee5270d61f48c89388cbf1f855f
Matthieu Laneuville <mlaneuville@gmail.com>
2016-03-17 14:28:10 +0900

    updated all_files.sh to match recent changes

--- all_files.sh
+++ all_files.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+# The command line argument 'folder' is optional. If set, the script produces the output
+# of all files in that subfolder.
 
-for file in ./out/*output.txt; do
+FOLDER=$1
+echo $FOLDER
+
+for file in ./out/${FOLDER}/*output.txt; do
     essai=${file%-out*}
     essai=${essai##*/}
 
+    if [ $FOLDER ]; then
+        python plot.py -s $FOLDER $essai -n5
+    else
+        python plot.py $essai -n5
+    fi
+    
     echo "===="
     echo $essai
-    python plot.py $essai 10
 done
